c2818bdb31d073f8bd2219840ce433dc64b4702e
donavan <donavan.stanley@gmail.com>
2014-11-09 15:58:28 -0500

    Use -d for directory tests not -f dumbass

--- flexget/config.sh
+++ flexget/config.sh
@@ -19,43 +19,43 @@ if [[ ! -f $CONFIG/settings.json ]]; then
 fi
 
 # Make sure the download folders exist
-if [[ ! -f $DOWNLOADS/tv ]]; then
+if [[ ! -d $DOWNLOADS/tv ]]; then
   echo "Creating folder for TV shows in ${DOWNLOADS}"
   mkdir $DOWNLOADS/tv
 fi
 
-if [[ ! -f $DOWNLOADS/movies ]]; then
+if [[ ! -d $DOWNLOADS/movies ]]; then
   echo "Creating folder for movies in ${DOWNLOADS}"
   mkdir $DOWNLOADS/movies
 fi
 
-if [[ ! -f $DOWNLOADS/audio ]]; then
+if [[ ! -d $DOWNLOADS/audio ]]; then
   echo "Creating folder for audio in ${DOWNLOADS}"
   mkdir $DOWNLOADS/audio
 fi
 
-if [[ ! -f $DOWNLOADS/other ]]; then
+if [[ ! -d $DOWNLOADS/other ]]; then
   echo "Creating folder for other content in ${DOWNLOADS}"
   mkdir $DOWNLOADS/other
 fi
 
 # Make sure we have folders for long term storage
-if [[ ! -f $CONTENT/tv ]]; then
+if [[ ! -d $CONTENT/tv ]]; then
   echo "Creating folder for TV shows in ${CONTENT}"
   mkdir $CONTENT/tv
 fi
 
-if [[ ! -f $CONTENT/movies ]]; then
+if [[ ! -d $CONTENT/movies ]]; then
   echo "Creating folder for movies in ${CONTENT}"
   mkdir $CONTENT/movies
 fi
 
-if [[ ! -f $CONTENT/audio ]]; then
+if [[ ! -d $CONTENT/audio ]]; then
   echo "Creating folder for audio in ${CONTENT}"
   mkdir $CONTENT/audio
 fi
 
-if [[ ! -f $CONTENT/other ]]; then
+if [[ ! -d $CONTENT/other ]]; then
   echo "Creating folder for other content in ${CONTENT}"
   mkdir $CONTENT/other
 fi
